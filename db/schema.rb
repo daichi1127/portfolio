e63dd7f816eed2472c03a44575906e91344c8632
EC2 Default User <ec2-user@ip-172-31-18-125.ap-northeast-1.compute.internal>
2020-10-23 02:57:38 +0000

    create contents scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_23_023347) do
+ActiveRecord::Schema.define(version: 2020_10_23_025146) do
+
+  create_table "contents", force: :cascade do |t|
+    t.string "title"
+    t.string "detail"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
